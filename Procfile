18f4194576ddc206d27cbd30acb16282a2bbd16b
Adam <adamh93@verizon.net>
2019-03-18 22:58:58 -0400

    done: Redo app structure so it can deploy to Heroku by moving contents of credit_calc folder into root and delete credit_calc folder

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: flask db upgrade; flask translate compile; gunicorn routes:app
+web: flask db upgrade; flask translate compile; gunicorn credit_calc:app
--- credit_calc.py
+++ credit_calc.py
@@ -1,5 +1,5 @@
 from app import app, db
-from app.models import Departments, Programs
+from app.models import Departments, Programs, Courses
 
 @app.shell_context_processor
 def make_shell_context():
